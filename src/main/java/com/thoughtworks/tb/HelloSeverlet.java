59d9bc36558d8da76f03404776e051cc6007eb9d
Lan Zhao <zl.dian@163.com>
2014-07-15 10:10:46 +0800

    add packet

--- src/main/java/com/thoughtworks/tb/HelloSeverlet.java
+++ src/main/java/com/thoughtworks/tb/HelloSeverlet.java
@@ -1,9 +1,12 @@
+package com.thoughtworks.tb;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 public class HelloSeverlet extends HttpServlet {
  
     private static final long serialVersionUID = 1L;
